1fd9e86dc61eefd7df69c2466e1b8b7ffb98f75c
izumizawa <mariana.izumizawa@matera.com>
2021-02-24 14:12:18 -0300

    🔧 fix: close icon color in hover

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -67,7 +67,16 @@ export function Countdown() {
               onClick={resetCountdown}
             >
               Abandonar ciclo
-              <img src="icons/close.svg" alt="Close" />
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className={styles.closeIcon}
+              >
+                <path d="M27 14.41L25.59 13L20 18.59L14.41 13L13 14.41L18.59 20L13 25.59L14.41 27L20 21.41L25.59 27L27 25.59L21.41 20L27 14.41Z" />
+              </svg>{" "}
             </button>
           ) : (
             <button
